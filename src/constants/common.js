173bace906f53d5d123877b444e09f289404519c
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2022-09-05 21:56:15 +0700

    reactjs commit

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,7 +1,7 @@
 export const TOKEN_CYBERSOFT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyOSIsIkhldEhhblN0cmluZyI6IjE5LzAxLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NDA4NjQwMDAwMCIsIm5iZiI6MTY0NTk4MTIwMCwiZXhwIjoxNjc0MjM0MDAwfQ.YESwad1hPeFZLi1alQUINpqBwiG-eLBBTADYwGZBfQc';
 export const BASE_URL ='https://movienew.cybersoft.edu.vn/api';
 export const USER_INFO_KEY = "USER_INFO_KEY";
-export const GROUP_ID = 'GP03';
+export const GROUP_ID = 'GP01';
 export const HOUR = ["09:00:00","12:00:00","15:00:00","18:00:00","21:00:00"];
 export const banner_theater = ['http://codienlanh.com/wp-content/uploads/2018/08/z956243662384_d93c08ef99c98bbfff72d797dee26919-1024x778.jpg',
 'https://cafefcdn.com/thumb_w/650/203337114487263232/2022/6/27/photo1656323960029-1656323961697468219282.jpg',
