327e5028964b7c740a91fb3f4f6ef456c02cfd3b
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-02-04 09:50:45 +0700

    fix access with admin role

--- src/guards/admin.guards.jsx
+++ src/guards/admin.guards.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { notification } from 'antd';
 import { MaLoaiNguoiDung } from "../enums/common";
 
 export default function AdminGuards() {
+  const [loadingState, setLoadingState] = useContext(LoadingContext);
   const { userInfor } = useSelector((state) => state.userReducer);
   const navigate = useNavigate();
 
@@ -15,6 +16,7 @@ export default function AdminGuards() {
         message: "Cảnh báo",
         description: "Khách hàng không thể truy cập vào trang Admin",
       });
+      setLoadingState({ isLoading: false });
       navigate("/login");
       return
     }
