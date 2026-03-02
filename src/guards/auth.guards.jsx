b46854cd6a92860acb515d1c2f185797e14e4e73
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-03-02 16:45:18 +0700

    test demo paymnent

--- src/guards/auth.guards.jsx
+++ src/guards/auth.guards.jsx
@@ -1,3 +1,8 @@
+import { useAuth } from "contexts/auth.context";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+
 export default function AuthGuards() {
   const userState = useSelector((state) => state.userReducer);
   const { openLogin } = useAuth();
