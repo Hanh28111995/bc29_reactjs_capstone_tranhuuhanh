113a35a3a87a6568e0e94e62eccb05d37af87068
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-03-02 16:42:40 +0700

    test demo paymnent

--- src/guards/auth.guards.jsx
+++ src/guards/auth.guards.jsx
@@ -8,9 +8,8 @@ export default function AuthGuards() {
     }
   }, [userState.userInfor, openLogin]);
 
-  // Nếu chưa đăng nhập, không trả về Outlet để tránh render trang bảo mật
   if (!userState.userInfor) {
-    return null; // Hoặc một trang Loading/thông báo
+    return null;
   }
 
   return <Outlet />;
--- src/pages/login/LoginModal.jsx
+++ src/pages/login/LoginModal.jsx
@@ -6,13 +6,12 @@ import { Modal } from 'antd';
 export default function LoginModal() {
     const { isLoginModalOpen, closeLogin } = useAuth();
 
-    if (!isLoginModalOpen) return null;
     return (
         <Modal
-            open={isLoginModalOpen} // Sử dụng prop 'open' thay vì 'return null' bên ngoài
-            onCancel={closeLogin}   // Gọi hàm đóng modal khi nhấn X hoặc nhấn ra ngoài
-            footer={null}           // Ẩn các nút OK/Cancel mặc định của Antd
-            centered                // Căn giữa màn hình            
+            open={isLoginModalOpen} 
+            onCancel={closeLogin}   
+            footer={null}           
+            centered                
         >
             <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
                 <div className="bg-white p-6 rounded-lg relative">
