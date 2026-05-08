270519db85f2f373653ff68e8010f21860d128a5
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-05-08 16:18:42 +0700

    connect db

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -3,7 +3,7 @@ import { BASE_URL, USER_INFO_KEY } from "../constants/common";
 
 export const request = axios.create({
   proxy: false,  
-  timeout: 10000,
+  timeout: 20000,
   baseURL: BASE_URL,
   withCredentials: true,
 });
