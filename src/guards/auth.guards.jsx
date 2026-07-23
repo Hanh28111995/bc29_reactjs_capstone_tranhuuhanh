abb4ac515c1fdb5d48c2af5de92c2d28288ae3aa
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-07-23 14:52:58 +0700

    fix guard

--- src/guards/auth.guards.jsx
+++ src/guards/auth.guards.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { notification } from 'antd';
 import { useAuth } from 'contexts/auth.context';
+
 
 export default function AuthGuards() {
   const userRole = useSelector((state) => state.userReducer.userInfor?.user_inf?.role);
   const { openLogin } = useAuth();
   const [checked, setChecked] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!userRole) {
@@ -15,7 +17,8 @@ export default function AuthGuards() {
         message: "Cảnh báo",
         description: "Bạn cần đăng nhập để tiếp tục đặt vé",
       });
-      openLogin();
+      // openLogin();
+       navigate(-1);
     }
     setChecked(true);
   }, [userRole]);
