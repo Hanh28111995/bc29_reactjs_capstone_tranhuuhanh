a3ac2844207e219c3fb8f7f61152ed8b99001247
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-13 19:55:30 +0700

    rerender

--- src/pages/heroPage/HeroPage.jsx
+++ src/pages/heroPage/HeroPage.jsx
@@ -8,7 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function HeroPage() {
-  // const [loadingState] = useContext(LoadingContext);
+  const [loadingState] = useContext(LoadingContext);
   const settings = {
     dots: false,
     infinite: false,
