f8c5d918d2b161f64fb7a3165dab4e39ce47a173
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-13 15:15:33 +0700

    rename HeroPage component

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -4,20 +4,14 @@ import GoToTop from "routes/goToTop";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import "./index.scss";
-import RightSidebar from "../components/sidebar/RightSideBar";
-import { useSelector } from "react-redux";
 
-export default function HomeLayout() {
-  const userRole = useSelector(
-    (state) => state.userReducer.userInfor?.user_inf?.role,
-  );
+export default function HomeLayout() {  
   return (
     <div
       className="wrapper d-flex flex-column justify-content-between"
       style={{ caretColor: "transparent" }}
     >
       <Header />
-      {/* {userRole && <RightSidebar />}       */}
       <Outlet />
       <Footer />
       <GoToTop />
