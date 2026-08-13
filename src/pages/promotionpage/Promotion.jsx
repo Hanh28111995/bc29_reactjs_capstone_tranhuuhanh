6f2e581e76b990aebafac49aab755213b8189f20
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-13 20:31:08 +0700

    loading

--- src/pages/promotionpage/Promotion.jsx
+++ src/pages/promotionpage/Promotion.jsx
@@ -1,17 +1,12 @@
-import React, { useContext } from 'react'
-import { Spin} from 'antd';
-import { LoadingContext } from 'contexts/loading.context';
+import React from "react";
+
 import "./../home/index.scss";
-import PromotionList from 'modules/itemLists/PromotionList';
+import PromotionList from "modules/itemLists/PromotionList";
 
 export default function Promotion() {
-  const [loadingState] = useContext(LoadingContext);
-
   return (
-    <Spin spinning={loadingState.isLoading} size="large">      
-      <div className='homePage'>        
-        <PromotionList />
-      </div>
-    </Spin>
-  )
+    <div className="homePage">
+      <PromotionList />
+    </div>
+  );
 }
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,18 +1,11 @@
-import React, { useContext } from 'react'
-import { Spin} from 'antd';
-import { LoadingContext } from 'contexts/loading.context';
+import React from "react";
 import "./../home/index.scss";
-import ShopProductList from 'modules/itemLists/ShopProductList';
-
+import ShopProductList from "modules/itemLists/ShopProductList";
 
 export default function Shop() {
-  const [loadingState] = useContext(LoadingContext);
-
   return (
-    <Spin spinning={loadingState.isLoading} size="large">      
-      <div className='homePage'>        
-        <ShopProductList />
-      </div>
-    </Spin>
-  )
+    <div className="homePage">
+      <ShopProductList />
+    </div>
+  );
 }
