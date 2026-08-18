a79aa2c995dc56b6e14c1ee8208c525caee6a009
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-19 04:11:47 +0700

    add  debounce search banner  table

--- src/modules/tables/BannersTable.jsx
+++ src/modules/tables/BannersTable.jsx
@@ -235,7 +235,7 @@ export default function BannerTable() {
                         >
                             <Button type="text" icon={<DeleteOutlined style={{ color: 'red' }} />} />
                         </Popconfirm>
-                    </div>
+                    </div>,
                 ];
             }
         }
