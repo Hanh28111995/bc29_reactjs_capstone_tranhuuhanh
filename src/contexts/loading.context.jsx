33df015db037e364a6717d81f46daf91aa2cf049
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-27 01:33:41 +0700

    loading without GloblaSpin

--- src/contexts/loading.context.jsx
+++ src/contexts/loading.context.jsx
@@ -1,31 +1,6 @@
-import { Spin } from "antd";
 import { createContext, useCallback, useRef, useState } from "react";
-import styled from "styled-components"; // Hoặc đường dẫn file styled của bạn
-
-const WrapperSpin = styled.div`
-  position: fixed;
-  inset: 0;
-  background-color: rgba(255, 255, 255, 0.1);
-  pointer-events: none;
-  z-index: 9999;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  transform: translateZ(0);
-`;
-
 const LoadingContext = createContext(null);
 
-const GlobalSpinner = ({ spinRef }) => {
-  const [visible, setVisible] = useState(false);
-  spinRef.current = setVisible;
-  return visible ? (
-    <WrapperSpin>
-      <Spin />
-    </WrapperSpin>
-  ) : null;
-};
 
 const LoadingProvider = (props) => {
   const spinRef = useRef(null);
@@ -45,8 +20,7 @@ const LoadingProvider = (props) => {
   const value = useRef([{ isLoading: false }, setState]);
 
   return (
-    <LoadingContext.Provider value={value.current}>
-      <GlobalSpinner spinRef={spinRef} />
+    <LoadingContext.Provider value={value.current}>      
       {props.children}
     </LoadingContext.Provider>
   );
