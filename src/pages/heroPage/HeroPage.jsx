17d5f9e1372e029abe6c165e5fe044767e28a383
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-09-02 23:10:27 +0700

    ux/ui header footer

--- src/pages/heroPage/HeroPage.jsx
+++ src/pages/heroPage/HeroPage.jsx
@@ -48,14 +48,45 @@ export default function HeroPage() {
     }
   };
   const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 3,
-    arrows: true,
-    swipe: true,
-  };
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: true,
+  swipe: true,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
   const settings_child = {
     dots: false,
     infinite: true,
